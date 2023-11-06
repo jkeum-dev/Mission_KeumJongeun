f9019ed3c635727803671791c2f5f6e91932d23c
jkeum-dev <jon9eun@gmail.com>
2023-11-06 13:19:06 +0900

    5단계까지 코드 리팩토링

--- src/main/java/com/tb/App.java
+++ src/main/java/com/tb/App.java
@@ -6,15 +6,16 @@ import java.util.Scanner;
 
 public class App {
 	private final Scanner sc;
+	private int lastId;
+	private List<Phrase> list;
 
 	public App() {
 		this.sc = new Scanner(System.in);
+		this.lastId = 0;
+		this.list = new ArrayList<>();
 	}
 
 	public void run() {
-		int lastId = 0;
-		List<Phrase> list = new ArrayList<>();
-
 		System.out.println("== 명언 앱 ==");
 		while (true) {
 			System.out.print("명령) ");
@@ -23,19 +24,27 @@ public class App {
 				sc.close();
 				System.exit(0);
 			} else if (cmd.equals("등록")) {
-				Phrase ph = new Phrase();
-				System.out.print("명언: ");
-				ph.setContent(sc.nextLine());
-				System.out.print("작가: ");
-				ph.setAuthor(sc.nextLine());
-				ph.setId(++lastId);
-				list.add(ph);
-				System.out.println(ph.getId() + "번 명령이 등록되었습니다.");
+				addPhrase();
 			} else if (cmd.equals("목록")) {
-				System.out.println("번호 / 작가 / 명언\n" + "----------------------");
-				for (Phrase o: list)
-					System.out.println(o.getId() + " / " + o.getAuthor() + " / " + o.getContent());
+				getList();
 			}
 		}
+	}
+
+	private void addPhrase() {
+		Phrase ph = new Phrase();
+		System.out.print("명언: ");
+		ph.setContent(sc.nextLine());
+		System.out.print("작가: ");
+		ph.setAuthor(sc.nextLine());
+		ph.setId(++lastId);
+		list.add(ph);
+		System.out.println(ph.getId() + "번 명령이 등록되었습니다.");
+	}
+
+	private void getList() {
+		System.out.println("번호 / 작가 / 명언\n" + "----------------------");
+		for (Phrase o: list)
+			System.out.println(o.getId() + " / " + o.getAuthor() + " / " + o.getContent());
 	}
 }
