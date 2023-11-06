568d5dbf5bf7bdc6e79fd981ac56346334b90c82
jkeum-dev <jon9eun@gmail.com>
2023-11-06 10:37:27 +0900

    1단계: 종료

--- src/main/java/com/tb/App.java
+++ src/main/java/com/tb/App.java
@@ -1,7 +1,20 @@
 package com.tb;
 
-public class App {
-	public void run() {
+import java.util.Scanner;
 
+public class App {
+	private final Scanner sc;
+
+	public App() {
+		this.sc = new Scanner(System.in);
+	}
+
+	public void run() {
+		System.out.println("== 명언 앱 ==");
+		String cmd = sc.nextLine();
+		if (cmd.equals("종료")) {
+			sc.close();
+			System.exit(0);
+		}
 	}
 }
